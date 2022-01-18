02e68b9512d98f0d232ef5057256389de9c66a84
Akash <aakash23kriplani@gmail.com>
2022-01-18 16:30:16 +0530

    Adding Generics to CsvFileReader

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,14 +1,12 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+export abstract class CsvFileReader<T> {
 
-export class CsvFileReader {
-
-  data: MatchData[] = [];
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(data: string[]): T;
 
   read(): void {
   this.data =  fs
@@ -17,18 +15,6 @@ export class CsvFileReader {
     })
     .split('\n')
     .map((row: string): string[] => row.split(','))
-    .map(
-      (row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3], 10),
-          parseInt(row[4], 10),
-          row[5] as MatchResult,
-          row[6]
-        ];
-      }
-    );
+    .map(this.mapRow);
   }
 }
